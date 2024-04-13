38a81c7ec2259bb78a75d399215a67af28b7e144
Mason Graves - UNR <143383761+1104-Graves-Mason@users.noreply.github.com>
2024-04-13 03:12:22 -0700

    main.c, with accurate detection
    commit 2

--- main.c
+++ main.c
@@ -47,14 +47,14 @@ int main(){
     Square* s1 = &square1;
     Square* s2 = &square2;
 //Rotate a square
-    int frames = 160;
+    int frames = 200;
     for(int i = 0; i < frames; i++){
         //Rotation and movement of Square 1
         square1.angle = (40*(pi/180))+i*(pi/180);
         square1.xCorner = 1+(i/10);
         square1.yCorner = 4+(i/10);
         //Rotation and movement of Square 2
-        square2.angle = (40*(pi/180))+i*(pi/180);
+        square2.angle = (40*(pi/180))+(i/2)*(pi/180);
         square2.xCorner = 35-(i/10);
         square2.yCorner = 28-(i/10);
         if(square1.angle == 0){
@@ -64,6 +64,9 @@ int main(){
             square2.angle = 0.005;
         }
         //placement of squares
+        if(separation(s1,s2)==true){
+            break;
+        }
         setSquare(s1, grid);
         setSquare(s2, grid);
         printGrid(grid);
@@ -73,7 +76,7 @@ int main(){
         emptySquare(s1, grid);
         emptySquare(s2, grid);
     }
-    separation(s1,s2);
+    printf("Collision detected - halting");
     return 0;
 }
 
@@ -111,7 +114,7 @@ void setSquare(Square* s, char g[GRIDSIZE][GRIDSIZE]){
         for(float j = 0; j < SIZE; j+=0.25){
             double ax1 = a+(SIZE/2)+((i-(SIZE/2))*cos(an)-(j-(SIZE/2))*sin(an));
             double ax2 = b+(SIZE/2)+((i-(SIZE/2))*sin(an)+(j-(SIZE/2))*cos(an));
-            if(ax1 < 0 || ax2 < 0 || ax1 > GRIDSIZE || ax2 > GRIDSIZE){
+            if(ax1 < 0 || ax2 < 0 || ax1 > GRIDSIZE|| ax2 > GRIDSIZE){
             }else{
                 setCoordinate('#', g, ax1, ax2);
             }
@@ -127,7 +130,7 @@ void emptySquare(Square* s, char g[GRIDSIZE][GRIDSIZE]){
         for(float j = 0; j < SIZE; j+=0.25){
             double ax1 = a+(SIZE/2)+((i-(SIZE/2))*cos(an)-(j-(SIZE/2))*sin(an));
             double ax2 = b+(SIZE/2)+((i-(SIZE/2))*sin(an)+(j-(SIZE/2))*cos(an));
-            if(ax1 < 0 || ax2 < 0 || ax1 > GRIDSIZE || ax2 > GRIDSIZE){
+            if(ax1 < 0 || ax2 < 0 || ax1 > GRIDSIZE|| ax2 > GRIDSIZE){
             }else{
                 setCoordinate('.', g, ax1, ax2);
             }
@@ -143,7 +146,7 @@ float cot(float theta){
 }
 
 float rTan(float theta){
-    if(cos(theta == 0)){
+    if(cos(theta) == 0){
         theta = (pi/180)*90.005;
     }
     return (sin(theta)/cos(theta));
@@ -158,20 +161,71 @@ bool separation(Square* s1, Square* s2){
     float s2p2c1 = (s2->xCorner)-(SIZE*sin(s2->angle))+cot(s1->angle)*(BOUNDARY-(s2->yCorner)-(SIZE*cos(s2->angle)));
     float s2p3c1 = (s2->xCorner)+(SIZE*cos(s2->angle))+cot(s1->angle)*(BOUNDARY-(s2->yCorner)-(SIZE*sin(s2->angle)));
     float s2p4c1 = (s2->xCorner)+(SIZE*(cos(s2->angle)-sin(s2->angle)))+cot(s1->angle)*(BOUNDARY-(s2->yCorner)-(SIZE*(cos(s2->angle)+sin(s2->angle))));
-    printf("%f\n%f\n%f\n%f\n%f\n%f\n\n\n", s1p1c1, s1p2c1, s2p1c1, s2p2c1, s2p3c1, s2p4c1);
-    if(!(s1p1c1 < s2p1c1 && s2p1c1 > s1p2c1 ||
-    s1p1c1 > s2p1c1 && s2p1c1 < s1p2c1 ||
-    s1p1c1 < s2p2c1 && s2p2c1 > s1p2c1 ||
-    s1p1c1 > s2p2c1 && s2p2c1 < s1p2c1 ||
-    s1p1c1 < s2p3c1 && s2p3c1 > s1p2c1 ||
-    s1p1c1 > s2p3c1 && s2p3c1 < s1p2c1 ||
-    s1p1c1 < s2p4c1 && s2p4c1 > s1p2c1 ||
-    s1p1c1 > s2p4c1 && s2p4c1 < s1p2c1)){
+    // printf("%f\n%f\n%f\n%f\n%f\n%f\n\n\n", s1p1c1, s1p2c1, s2p1c1, s2p2c1, s2p3c1, s2p4c1);
+    if(!(s1p1c1 < s2p1c1 && s2p1c1 < s1p2c1 ||
+    s1p1c1 > s2p1c1 && s2p1c1 > s1p2c1 ||
+    s1p1c1 < s2p2c1 && s2p2c1 < s1p2c1 ||
+    s1p1c1 > s2p2c1 && s2p2c1 > s1p2c1 ||
+    s1p1c1 < s2p3c1 && s2p3c1 < s1p2c1 ||
+    s1p1c1 > s2p3c1 && s2p3c1 > s1p2c1 ||
+    s1p1c1 < s2p4c1 && s2p4c1 < s1p2c1 ||
+    s1p1c1 > s2p4c1 && s2p4c1 > s1p2c1)){
         return false;
     }
-    //condition 2 for shape 2
+    //condition 2 for shape 1
     float s1p1c2 = (s1->xCorner)-rTan(s1->angle)*(BOUNDARY-(s1->yCorner));
     float s1p2c2 = (s1->xCorner)+(SIZE*cos(s1->angle))-rTan(s1->angle)*(BOUNDARY-(s1->yCorner)-SIZE*sin(s1->angle));
-    
-    return false;
+    float s2p1c2 = (s2->xCorner)-rTan(s1->angle)*(BOUNDARY-(s2->yCorner));
+    float s2p2c2 = (s2->xCorner)-(SIZE*sin(s2->angle))-rTan(s1->angle)*(BOUNDARY-(s2->yCorner)-SIZE*cos(s2->angle));
+    float s2p3c2 = (s2->xCorner)+(SIZE*cos(s2->angle))-rTan(s1->angle)*(BOUNDARY-(s2->yCorner)-SIZE*sin(s2->angle));
+    float s2p4c2 = (s2->xCorner)-(SIZE*(sin(s2->angle)-cos(s2->angle)))-rTan(s1->angle)*(BOUNDARY-(s2->yCorner)-SIZE*(sin(s2->angle)+cos(s2->angle)));
+    // printf("%f\n%f\n%f\n%f\n%f\n%f\n\n\n", s1p1c2, s1p2c2, s2p1c2, s2p2c2, s2p3c2, s2p4c2);
+    if(!(s1p1c2 < s2p1c2 && s2p1c2 < s1p2c2 ||
+    s1p1c2 > s2p1c2 && s2p1c2 > s1p2c2 ||
+    s1p1c2 < s2p2c2 && s2p2c2 < s1p2c2 ||
+    s1p1c2 > s2p2c2 && s2p2c2 > s1p2c2 ||
+    s1p1c2 < s2p3c2 && s2p3c2 < s1p2c2 ||
+    s1p1c2 > s2p3c2 && s2p3c2 > s1p2c2 ||
+    s1p1c2 < s2p4c2 && s2p4c2 < s1p2c2 ||
+    s1p1c2 > s2p4c2 && s2p4c2 > s1p2c2)){
+        return false;
+    }
+    //shape 2 is now key
+    //condition 3 for shape 2
+    float s2p1c3 = (s2->xCorner)+cot(s2->angle)*(BOUNDARY-(s2->yCorner));
+    float s2p2c3 = (s2->xCorner)-(SIZE*sin(s2->angle))+cot(s2->angle)*(BOUNDARY-(s2->yCorner)-(SIZE*cos(s2->angle)));
+    float s1p1c3 = (s1->xCorner)+cot(s2->angle)*(BOUNDARY-(s1->yCorner));
+    float s1p2c3 = (s1->xCorner)-(SIZE*sin(s1->angle))+cot(s2->angle)*(BOUNDARY-(s1->yCorner)-(SIZE*cos(s1->angle)));
+    float s1p3c3 = (s1->xCorner)+(SIZE*cos(s1->angle))+cot(s2->angle)*(BOUNDARY-(s1->yCorner)-(SIZE*sin(s1->angle)));
+    float s1p4c3 = (s1->xCorner)+(SIZE*(cos(s1->angle)-sin(s1->angle)))+cot(s2->angle)*(BOUNDARY-(s1->yCorner)-(SIZE*(cos(s1->angle)+sin(s1->angle))));
+    // printf("%f\n%f\n%f\n%f\n%f\n%f\n\n\n", s2p1c3, s2p2c3, s1p1c3, s1p2c3, s1p3c3, s1p4c3);
+    if(!(s2p1c3 < s1p1c3 && s1p1c3 < s2p2c3 ||
+    s2p1c3 > s1p1c3 && s1p1c3 > s2p2c3 ||
+    s2p1c3 < s1p2c3 && s1p2c3 < s2p2c3 ||
+    s2p1c3 > s1p2c3 && s1p2c3 > s2p2c3 ||
+    s2p1c3 < s1p3c3 && s1p3c3 < s2p2c3 ||
+    s2p1c3 > s1p3c3 && s1p3c3 > s2p2c3 ||
+    s2p1c3 < s1p4c3 && s1p4c3 < s2p2c3 ||
+    s2p1c3 > s1p4c3 && s1p4c3 > s2p2c3)){
+        return false;
+    }
+    //condition 4 for shape 2
+    float s2p1c4 = (s2->xCorner)-rTan(s2->angle)*(BOUNDARY-(s2->yCorner));
+    float s2p2c4 = (s2->xCorner)+(SIZE*cos(s2->angle))-rTan(s2->angle)*(BOUNDARY-(s2->yCorner)-SIZE*sin(s2->angle));
+    float s1p1c4 = (s1->xCorner)-rTan(s2->angle)*(BOUNDARY-(s1->yCorner));
+    float s1p2c4 = (s1->xCorner)-(SIZE*sin(s1->angle))-rTan(s2->angle)*(BOUNDARY-(s1->yCorner)-SIZE*cos(s1->angle));
+    float s1p3c4 = (s1->xCorner)+(SIZE*cos(s1->angle))-rTan(s2->angle)*(BOUNDARY-(s1->yCorner)-SIZE*sin(s1->angle));
+    float s1p4c4 = (s1->xCorner)-(SIZE*(sin(s1->angle)-cos(s1->angle)))-rTan(s2->angle)*(BOUNDARY-(s1->yCorner)-SIZE*(sin(s1->angle)+cos(s1->angle)));
+    printf("%f\n%f\n%f\n%f\n%f\n%f\n\n\n", s2p1c4, s2p2c4, s1p1c4, s1p2c4, s1p3c4, s1p4c4);
+    if(!(s2p1c4 < s1p1c4 && s1p1c4 < s2p2c4 ||
+    s2p1c4 > s1p1c4 && s1p1c4 > s2p2c4 ||
+    s2p1c4 < s1p2c4 && s1p2c4 < s2p2c4 ||
+    s2p1c4 > s1p2c4 && s1p2c4 > s2p2c4 ||
+    s2p1c4 < s1p3c4 && s1p3c4 < s2p2c4 ||
+    s2p1c4 > s1p3c4 && s1p3c4 > s2p2c4 ||
+    s2p1c4 < s1p4c4 && s1p4c4 < s2p2c4 ||
+    s2p1c4 > s1p4c4 && s1p4c4 > s2p2c4)){
+        return false;
+    }
+    return true;
 }
